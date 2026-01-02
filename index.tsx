7e376b4c6c9bb24b99ab67732d9737d70bf2d185
keyu-wan <higinnywky@gmail.com>
2026-01-03 02:52:07 +0800

    Remove unnecessary comment from index.tsx

--- index.tsx
+++ index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-#
+
 const rootElement = document.getElementById('root');
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
